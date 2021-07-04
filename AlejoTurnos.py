00418581fe0b6b767415011c886b8df644f80077
Alejo8139 <poisondrakev@gmail.com>
2021-07-04 20:08:06 -0300

    Actualizacion

--- AlejoTurnos.py
+++ AlejoTurnos.py
@@ -1,32 +1,35 @@
 import random
 
-
-def nombres():
+def pedir_nombres(max_nombres):
     """
-    Crea un diccionario con los nombres de los jugadores y luego lo
-    retorna ordenado al azar. el valor de cada nombre sera 6 espacios
-    para los aciertos, desaciertos, etc.
-
-    firma: Alejo
-    """
-    max_nombres = 5
-    Jugadores = []
-    DiccionarioJugadores = {}
+Pide nombres con una longitud maxima de los mismos y retorna una lista
+con los nombres al azar
+"""
+    n_lista = []
     nombre = "Aqui iran los nombres"
-    while nombre != "" and len(Jugadores) < max_nombres:
-        nombre = input("Ingrese el nombre del jugador {}: ".format(len(Jugadores) + 1))
-        if nombre == "" and len(Jugadores) == 0:
-            while len(Jugadores) == 0:
-                nombre = input("Por favor, ingrese al menos un nombre: ")
-                Jugadores.append(nombre) if nombre != "" else None
-        elif nombre in Jugadores:
-            print("Ingrese otro nombre por favor")
-        elif nombre == "":
+    while len(n_lista) < max_nombres and nombre != "":
+        nombre = input("Ingrese un nombre {}: ".format(len(n_lista) + 1))
+        if nombre == "" and len(n_lista) == 0:
+            while len(n_lista) == 0:
+                nombre = input("Ingrese al menos un nombre: ")
+                n_lista.append(nombre) if nombre != "" else None
+        elif nombre in n_lista:
+            print("Ingrese un nombre distinto")
             pass
-        else:
-            Jugadores.append(nombre)
-    random.shuffle(Jugadores)
-    for Jugador in Jugadores:
+        elif nombre != "":
+            n_lista.append(nombre)
+    random.shuffle(n_lista)
+    return n_lista
+
+def dict_nombres(Lista_nombres, Ganador = None):
+    """
+Con una lista de nombres predefinida, devuelve un diccionario con los jugadores
+"""
+    DiccionarioJugadores = {}
+    if Ganador != None:
+        Lista_nombres.remove(Ganador)
+        DiccionarioJugadores[Ganador] = ["","",0,0,"","",0]
+    for Jugador in Lista_nombres:
         DiccionarioJugadores[Jugador] = ["","",0,0,"","",0]
     return DiccionarioJugadores
 
@@ -37,20 +40,15 @@ def nueva_organizacion(DiccionarioJugadores, Ganador = None):
     """
     turnos_nuevos = {}
     Jugadores = list(DiccionarioJugadores.keys())
+    random.shuffle(Jugadores)
     if Ganador == None:
-        random.shuffle(Jugadores)
-        for Jugador in Jugadores:
-            turnos_nuevos[Jugador] = ["","",0,0,"","",0]
+        turnos_nuevos = dict_nombres(Jugadores)
     else:
-        Jugadores.remove(Ganador)
-        random.shuffle(Jugadores)
-        turnos_nuevos[Ganador] = ["","",0,0,"","",0]
-        for Jugador in Jugadores:
-            turnos_nuevos[Jugador] = ["","",0,0,"","",0]
+        turnos_nuevos = dict_nombres(Jugadores, Ganador)
     return turnos_nuevos
     
 
-def puntaje(DiccionarioJugadores, Puntos = None):
+def puntaje(DiccionarioJugadores, Puntos = {}):
     """
     De acuerdo al puntaje de los jugadores (hayan jugado o no) se adjunta los
     puntos de cada jugador en un diccionario donde las llaves sera cada jugador
@@ -59,7 +57,6 @@ def puntaje(DiccionarioJugadores, Puntos = None):
     firma: Alejo
     """
     nombres = list(DiccionarioJugadores.keys())
-    Puntos = {} if Puntos == None else Puntos
     if len(Puntos) == 0: 
         for Jugador in nombres:
             Puntos[Jugador] = DiccionarioJugadores[Jugador][6]
@@ -67,4 +64,3 @@ def puntaje(DiccionarioJugadores, Puntos = None):
         for Jugador in nombres:
             Puntos[Jugador] += DiccionarioJugadores[Jugador][6]
     return Puntos
-
--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def config():
     """
 Retorna un diccionario, teniendo como keys las palabras claves de configuracion y
@@ -6,18 +5,12 @@ como values, los valores de cada palabra clave
 
 Firma: Alejo
 """
-    long_deseada = 7
-    confi_dict = {}
-    posicion = 0
-    with open("Configuraciones.csv", "r+") as conf:
+    confi_dict = {"MAX_USUARIOS": 10,"LONG_PALABRA_MIN": 5,"MAX_DESACIERTOS": 7,"PUNTOS_ACIERTOS": 10,"PUNTOS_DESACIERTOS": 5,"PUNTOS_ADIVINA_PALABRA": 100,"PUNTOS_RESTA_GANA_PROGRAMA": 2}
+    with open("configuracion.csv", "r+") as conf:
         for linea in conf:
+            print(linea)
             texto_limpiado = limpieza(linea)
-            lista, posicion = busqueda_error(texto_limpiado, posicion)
-            confi_dict[lista[0]] = lista[1]
-            
-        while posicion < 7:
-            lista, posicion = busqueda_error([], posicion)
-            confi_dict[lista[0]] = lista[1]
+            confi_dict = cambio_valores(confi_dict, texto_limpiado)
     return confi_dict
 
 def limpieza(texto):
@@ -32,29 +25,15 @@ Firma: Alejo
         texto[1] = int(texto[1])
     return texto
 
-def busqueda_error(lista, posicion):
+def cambio_valores(confi_dict, lista):
     """
-Busca errores en la lista extraida del archivo csv, de presentar errores se colocan los valores por defecto
+Cambia valores de la lista default si cumple con ciertos requisitos, luego la devuelve
 
-Firma: Alejo
+Firma: Alejo, FedeBacelar
 """
-    n_def = ["MAX_USUARIOS","LONG_PALABRA_MIN","MAX_DESACIERTOS","PUNTOS_ACIERTOS","PUNTOS_DESACIERTOS","PUNTOS_ADIVINA_PALABRA","PUNTOS_RESTA_GANA_PROGRAMA"]
-    v_def = [10, 5, 7, 10, 5, 100, 2]
-    if len(lista) != 2:
-        lista_aprobada = [n_def[posicion], v_def[posicion]]
-    elif len(lista) == 2:
-            lista[0] = n_def[posicion] if lista[0] != n_def[posicion] else lista[0]
-            lista[1] = v_def[posicion] if type(lista[1]) is not int else lista[1]
-            lista_aprobada = lista
-    posicion += 1
-    return lista_aprobada, posicion
-
-lista = ["LONG_PALABRA_MIN", 87]
-posicion = 1
-lista, pos = busqueda_error(lista, posicion)
-print(config())
-=======
-"""
-Config en desarrollo
-"""
->>>>>>> 1e1d5f5917a236ee69e416470a7de83f3d44e407
+    
+    if len(lista) == 2:
+        for key in confi_dict:
+            if key == lista[0] and type(lista[1]) == int:
+                confi_dict[key] = lista[1]
+    return confi_dict
