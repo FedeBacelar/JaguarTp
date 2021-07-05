7c292b507f4e27c9315de2fc2c8168d158ea9085
Alejo8139 <poisondrakev@gmail.com>
2021-07-04 22:13:13 -0300

    Actualizacion y adicion de VariasVariables.py

---
+++ VariablesVarias.py
@@ -0,0 +1,43 @@
+import csv
+
+
+def cambio_conf(default = {}):
+    """
+ Te pregunta si desea cambiar los valores por default de las configuraciones,
+en caso de ser afirmativo este se apoyara de la funcion graf_conf() para
+tener una referencia de lo que el usuario desea cambiar. Al final retornara
+
+Firma: Alejo
+"""
+    sigue = True
+    while sigue:
+        cambio = input("Ingrese el parametro que desee cambiar: ")
+        while cambio < "1" and cambio > "7" or not cambio.isnumeric():
+            cambio = input("Ingrese un numero entre 0 y 7: ")
+        cambio_valor = input("Ingrese el nuevo valor: ")
+        parametros = graf_conf()
+        default[parametros[int(cambio)]] = int(cambio_valor)
+        eleccion = input("¿Desea cambiar otro valor?[s/n]: ")
+        while not eleccion.isalpha() or eleccion.lower() != "n" and eleccion.lower() != "s":
+            eleccion = input("Por favor ingrese 's' o 'n': ")
+        sigue = True if eleccion.lower() == 's' else False
+    return default
+    
+
+def graf_conf(parametros = {}):
+    """
+Te brinda un diccionario con los nombres de los parametros con un numero
+de referencia a cada uno.
+
+Firma: Alejo
+"""
+    with open("configuracion.csv", "r") as conf:
+        lectura = csv.reader(conf, delimiter=',')
+        for parametro in lectura:
+            parametros[len(parametros)+1]= parametro[0] 
+    return parametros
+
+conf = {'MAX_USUARIOS': 10, 'LONG_PALABRA_MIN': 5, 'MAX_DESACIERTOS': 7, 'PUNTOS_ACIERTOS': 10, 'PUNTOS_DESACIERTOS': 5, 'PUNTOS_ADIVINA_PALABRA': 100, 'PUNTOS_RESTA_GANA_PROGRAMA': 20}
+print(graf_conf())
+print(cambio_conf(conf))
+        
--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def config():
     """
 Retorna un diccionario, teniendo como keys las palabras claves de configuracion y
@@ -7,9 +6,8 @@ como values, los valores de cada palabra clave
 Firma: Alejo
 """
     confi_dict = {"MAX_USUARIOS": 10,"LONG_PALABRA_MIN": 5,"MAX_DESACIERTOS": 7,"PUNTOS_ACIERTOS": 10,"PUNTOS_DESACIERTOS": 5,"PUNTOS_ADIVINA_PALABRA": 100,"PUNTOS_RESTA_GANA_PROGRAMA": 2}
-    with open("configuracion.csv", "r+") as conf:
+    with open("configuracion.csv", "r") as conf:
         for linea in conf:
-            print(linea)
             texto_limpiado = limpieza(linea)
             confi_dict = cambio_valores(confi_dict, texto_limpiado)
     return confi_dict
@@ -38,60 +36,5 @@ Firma: Alejo, FedeBacelar
             if key == lista[0] and type(lista[1]) == int:
                 confi_dict[key] = lista[1]
     return confi_dict
-=======
-def config():
-    """
-    Retorna un diccionario, teniendo como keys las palabras claves de configuracion y
-    como values, los valores de cada palabra clave
 
-    Firma: Alejo
-    """
-    long_deseada = 7
-    confi_dict = {}
-    posicion = 0
-    with open("Configuraciones.csv", "r+") as conf:
-        for linea in conf:
-            texto_limpiado = limpieza(linea)
-            lista, posicion = busqueda_error(texto_limpiado, posicion)
-            confi_dict[lista[0]] = lista[1]
-            
-        while posicion < 7:
-            lista, posicion = busqueda_error([], posicion)
-            confi_dict[lista[0]] = lista[1]
-    return confi_dict
-
-def limpieza(texto):
-    """
-    Brinda una lista el conforme el texto de la configuracion dado.
-    Dejandolo en optimas condiciones para su uso en config()
-
-    Firma: Alejo
-    """
-    texto = texto.split(",")
-    if len(texto) > 1:
-        texto[1] = int(texto[1])
-    return texto
-
-def busqueda_error(lista, posicion):
-    """
-    Busca errores en la lista extraida del archivo csv, de presentar errores se colocan los valores por defecto
-
-    Firma: Alejo
-    """
-    n_def = ["MAX_USUARIOS","LONG_PALABRA_MIN","MAX_DESACIERTOS","PUNTOS_ACIERTOS","PUNTOS_DESACIERTOS","PUNTOS_ADIVINA_PALABRA","PUNTOS_RESTA_GANA_PROGRAMA"]
-    v_def = [10, 5, 7, 10, 5, 100, 2]
-    if len(lista) != 2:
-        lista_aprobada = [n_def[posicion], v_def[posicion]]
-    elif len(lista) == 2:
-            lista[0] = n_def[posicion] if lista[0] != n_def[posicion] else lista[0]
-            lista[1] = v_def[posicion] if type(lista[1]) is not int else lista[1]
-            lista_aprobada = lista
-    posicion += 1
-    return lista_aprobada, posicion
-
-
- # Falta reparar unos bugs cuando se borra una linea de la configuracion.csv pero para cambios erroneos en una linea funciona perfecto. Los problemas
- # surgen cuando se borra una linea de la configuracion.csv pues ahi reescribira los datos y asignara valores de forma ordenada cuando la realidad es
- # que el archivo al estar desordenado, asignara valores ordenados lo cual es indeseado para el jugador
-
->>>>>>> 564390a5904a6f4968425d62bcaeb4571d8efabb
+print(config())
